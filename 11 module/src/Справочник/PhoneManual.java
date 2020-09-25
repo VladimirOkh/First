78db649d668c270f792f9cb64d9b88cdb58062e0
Vladimir <ohremenkov@gmail.com>
2020-09-25 22:40:05 +0300

    Добавлены некоторые коррекции в метод main, часть его функций делегированно в метод checkName, создан метод для форматирования номера телефона

--- 11 module/src/Справочник/PhoneManual.java
+++ 11 module/src/Справочник/PhoneManual.java
@@ -6,38 +6,38 @@ import java.util.Scanner;
 public class PhoneManual {
     public static String[][] Manual = new String[5][1];
     public static String phone;
+    public static String name = null;
+    public static boolean isCorrectName = false;
+    public static Scanner scanner = new Scanner(System.in);
 
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        for (int i = 1; i < n; i++) {
-            boolean isCorrectName = false;
-            while (!isCorrectName) {
-                System.out.println("Введите ФИО " + i);
-                String name = scanner.nextLine();
-                isCorrectName = checkName(name);
-                if (!isCorrectName)
-                    System.out.println("Введите корректное имя!");
-                else {
-                    addPerson(Manual,name,phone);
-                }
-            }
+            System.out.println("Введите ФИО");
+            name = scanner.nextLine();
+            checkName(name);
+
+    }
+
+
+
+
+    private static void formatNumber(String name, String phoneNumber) {
+        String clean = phoneNumber.replaceAll("[^0-9]", "");
+        String result = "+7" + " " + clean.substring(1, 4) + " " +
+                clean.substring(4, 7) + " " + clean.substring(7, 9) + " " + clean.substring(9);
+    }
+
+    private static void checkName(String name) {
+        int blockCount = name.trim().split(" ").length;
+        PhoneManual.isCorrectName = blockCount == 3;
+        if (PhoneManual.isCorrectName) {
+            System.out.println("Введите номер");
+            phone = scanner.nextLine();
+            formatNumber(name, phone);
+        } else {
+            System.out.println("Введите корректное имя!");
+            checkName(name = scanner.nextLine());
         }
     }
-    public static String[][] addPerson(String Manual[][],String name,String phone){
-
-        return String[][];
-    }
-
-
-
-
-
-    private static boolean checkName(String name) {
-        boolean isTrue;
-        int blockCount = name.trim().split(" ").length;
-        isTrue = blockCount == 3;
-        return isTrue;
-    }
 }
+
