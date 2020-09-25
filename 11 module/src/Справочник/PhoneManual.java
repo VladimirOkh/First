03010c512039a621a86afb426e6118a89c3595ba
Vladimir <ohremenkov@gmail.com>
2020-09-25 20:06:40 +0300

    Добавлены массивы имен и номеров, main добавляет имя в names

--- 11 module/src/Справочник/PhoneManual.java
+++ 11 module/src/Справочник/PhoneManual.java
@@ -1,24 +1,35 @@
 package Справочник;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PhoneManual {
+    public static String[] names = new String[5];
+    public static long[] phones = new long[5];
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean isCorrectName = false;
         while (!isCorrectName) {
-            String name = scanner.nextLine(); //Считывает строку из System.in
+            System.out.println("Введите ФИО");
+            String name = scanner.nextLine();
             isCorrectName = checkName(name);
             if (!isCorrectName) System.out.println("Введите корректное имя!");
+            else {
+                for (int i = 0; i < names.length; i++) {
+                    if ((names[i] == null)) names[i] = name;
+                    System.out.println(Arrays.toString(names));
+                    break;
+                }
+            }
         }
     }
 
-        private static boolean checkName(String name) {
-            boolean isTrue;
-            System.out.println("Введите ФИО");
-            int blockCount = name.trim().split(" ").length;
-            isTrue = blockCount == 3;
-            return isTrue;
-        }
 
+    private static boolean checkName(String name) {
+        boolean isTrue;
+        int blockCount = name.trim().split(" ").length;
+        isTrue = blockCount == 3;
+        return isTrue;
+    }
 }
