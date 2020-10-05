a3fa2b8af5d4f5f50fba3bbc39d44e3c085a17db
Vladimir <ohremenkov@gmail.com>
2020-10-05 23:36:14 +0300

    Тренировка на исправление конфликтов 2

---
+++ untitled/src/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+    public void pet() {
+        System.out.println("Cat purrs");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Cat bella = new Cat();
+        bella.pet();
+    }
+}
