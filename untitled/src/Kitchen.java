5991a21e4835b8247a2a34b70cab7443058d1026
Vladimir <ohremenkov@gmail.com>
2020-10-05 19:50:26 +0300

    Тест конфликтов

--- untitled/src/Kitchen.java
+++ untitled/src/Kitchen.java
@@ -2,15 +2,16 @@ public class Kitchen {
     public void cookPasta() {
         System.out.println("Вскипятить воду");
         System.out.println("Положить в воду макароны");
+        System.out.println("Посолить"); //Новая строка
         System.out.println("Подождать");
         System.out.println("Слить воду");
     }
+}
 
-    public static class Main {
-        public static void main(String[] args) {
-            Kitchen kitchen = new Kitchen();
-            System.out.println("Варим макароны:");
-            kitchen.cookPasta();
-        }
+class Main {
+    public static void main(String[] args) {
+        Kitchen kitchen = new Kitchen();
+        System.out.println("Варим макароны:");
+        kitchen.cookPasta();
     }
 }
