f3e190fc16aa9226a142d8210616dd5971c49ab5
Vladimir <ohremenkov@gmail.com>
2020-10-17 16:11:32 +0300

    Доделана опция снятие денег

--- BankApp/src/Application.java
+++ BankApp/src/Application.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("=====================================");
+        System.out.println("---------------------");;
         System.out.println("Введите имя");
-        System.out.println("=====================================");
+        System.out.println("---------------------");;
         String name = sc.nextLine();
-        System.out.println("=====================================");
+        System.out.println("---------------------");;
         System.out.println("Введите ID");
-        System.out.println("=====================================");
+        System.out.println("---------------------");;
         String id = sc.nextLine();
         BankAccount account = new BankAccount(name,id);
+        account.menu();
     }
 }
--- BankApp/src/BankAccount.java
+++ BankApp/src/BankAccount.java
@@ -33,12 +33,12 @@ public class BankAccount {
         } else System.out.println("Предыдущие транзакции отсутствуют");
     }
 
-    public void Menu() {
+    public void menu() {
         char option = '\0';
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Здравствуйте " + customerName);
-        System.out.println("Ваш " + customerID);
+        System.out.println("Ваш ID " + customerID);
         System.out.println("\n");
         System.out.println("A. Проверить баланс");
         System.out.println("B. Положить деньги");
@@ -59,6 +59,11 @@ public class BankAccount {
                     System.out.println("Баланс: " + balance);
                     System.out.println("---------------------");
                     System.out.println("\n");
+                    System.out.println("A. Проверить баланс");
+                    System.out.println("B. Положить деньги");
+                    System.out.println("C. Снять деньги");
+                    System.out.println("D. Узнать предыдущую транзакцию");
+                    System.out.println("E. Выйти");
                     break;
                 case 'B':
                     System.out.println("---------------------");
@@ -67,23 +72,53 @@ public class BankAccount {
                     int amount = sc.nextInt();
                     deposit(amount);
                     System.out.println("\n");
+                    System.out.println("A. Проверить баланс");
+                    System.out.println("B. Положить деньги");
+                    System.out.println("C. Снять деньги");
+                    System.out.println("D. Узнать предыдущую транзакцию");
+                    System.out.println("E. Выйти");
                     break;
                 case 'C':
-                    System.out.println("---------------------");
-                    System.out.println("Введите сумму для снятия");
-                    System.out.println("---------------------");
-                    int amount2 = sc.nextInt();
-                    withdraw(amount2);
-                    System.out.println("\n");
-                    break;
+                    if (balance > 0) {
+                        System.out.println("---------------------");
+                        System.out.println("Введите сумму для снятия");
+                        System.out.println("---------------------");
+                        int amount2 = sc.nextInt();
+                        if (amount2 > balance) {
+                            System.out.println("Недостаточно средств на балансе! Вы не можете снять больше чем: " + balance + ". Пожалуйста пополните кошелек.");
+                            break;
+                        }
+                        withdraw(amount2);
+                        System.out.println("\n");
+                        System.out.println("A. Проверить баланс");
+                        System.out.println("B. Положить деньги");
+                        System.out.println("C. Снять деньги");
+                        System.out.println("D. Узнать предыдущую транзакцию");
+                        System.out.println("E. Выйти");
+                        break;
+                    } else {
+                        System.out.println("Недостаточно средств на балансе! Пожалуйста пополните кошелек.");
+                        break;
+                    }
                 case 'D':
                     System.out.println("---------------------");
                     getPreviousTransaction();
                     System.out.println("---------------------");
                     System.out.println('\n');
+                    System.out.println("A. Проверить баланс");
+                    System.out.println("B. Положить деньги");
+                    System.out.println("C. Снять деньги");
+                    System.out.println("D. Узнать предыдущую транзакцию");
+                    System.out.println("E. Выйти");
+                    break;
+                case 'E':
+                    System.out.println("*******************************");
+                    break;
+                default:
+                    System.out.println("Неверная опция. Пожалуйста попробуйте еще раз.");
                     break;
             }
-        }while (option != 'E');
+        } while (option != 'E');
         System.out.println("Всего доброго!");
     }
 }
